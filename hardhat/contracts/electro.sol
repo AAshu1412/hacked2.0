0bea80181edcd3d6d92f915c0d3ebae4af169998
AAshu1412 <ashutoshsanjeevmittal@gmail.com>
2024-03-02 02:43:22 +0530

    add hardhat

---
+++ hardhat/contracts/electro.sol
@@ -0,0 +1,105 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+
+contract electro is ERC20, ERC20Burnable, ERC20Permit {
+    address private owner;
+
+    struct people {
+        address people_address;
+        uint256 waste;
+        uint256 id;
+        uint256 amount;
+        uint256 total_amount;
+        uint256 amount_deducted;
+    }
+
+    struct Data {
+        address from;
+        address to;
+        uint256 value;
+    }
+    mapping(address => uint256[]) private map_uint;
+    // mapping(uint => address) private map_address;
+    mapping(uint256 => Data) private map_data;
+
+    mapping(address => people) public People;
+
+    mapping(address => string) private map_type;
+
+
+    constructor() ERC20("ElectroLite", "ETL") ERC20Permit("MyToken") {
+        _mint(msg.sender, 1000);
+        owner = msg.sender;
+        map_type[msg.sender]="Government";
+    }
+
+    modifier restricted() {
+        require(msg.sender == msg.sender);
+        _;
+    }
+
+    function sendReward(
+        address to,
+        uint256 value,
+        uint256 _waste
+    ) public restricted {
+        require(balanceOf(msg.sender) > 0, "Not enough Funds");
+        People[to].people_address = to;
+        People[to].waste = People[to].waste + _waste;
+        People[to].id = People[to].id + 1;
+        People[to].amount = value;
+        People[to].total_amount = People[to].total_amount + People[to].amount;
+        map_type[to]="User";
+        Data memory data1 = Data(msg.sender, to, value);
+        map_uint[to].push(People[to].id);
+        //   map_address[People[to].id]=to;
+        map_data[People[to].id] = data1;
+
+        transfer(to, value);
+    }
+
+    function people_spend(address to, uint256 value) public {
+        require(balanceOf(msg.sender) > 0, "Not enough Funds");
+        People[msg.sender].amount_deducted =
+            People[msg.sender].amount_deducted +
+            value;
+        People[to].id = People[to].id + 1;
+
+        Data memory data1 = Data(msg.sender, to, value);
+        map_uint[to].push(People[to].id);
+        // map_address[People[to].id]=msg.sender;
+        map_data[People[to].id] = data1;
+        transfer(to, value);
+    }
+
+    function people_total_amount(address _address) public view returns (uint256) {
+        return People[_address].total_amount;
+    }
+
+    function people_deducted_amount(address _address) public view returns (uint256) {
+        return People[_address].amount_deducted;
+    }
+
+    function people_waste(address _address) public view returns (uint256) {
+        return People[_address].waste;
+    }
+
+     function types(address _address) public view returns (string memory) {
+        return map_type[_address];
+    }
+   
+    function fetching_log_int(address _address) public view returns (uint256[] memory) {
+        return map_uint[_address];
+    }
+
+    function fetching_log_data(uint256 _id) public view returns (Data memory) {
+        // require(map_address[_id]==msg.sender,"You are not");
+        //     require(map_address[_id]==msg.sender,"dasda");
+
+        return map_data[_id];
+    }
+}
--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,46 +1,95 @@
 export default function About() {
-  
-
   return (
     <div className="flex flex-col gap-12 mx-20 mt-20">
-    <h1 className="text-4xl font-bold underline">About Us</h1>
-    <div className="text-xl flex flex-col gap-8">
-
-<div className="flex flex-col gap-3">
-<h1 className="text-2xl font-medium underline">Mission</h1>
-<h1>  - Our mission is to revolutionize waste management and energy production by providing an innovative platform that incentivizes sustainable practices and fosters community engagement.</h1>
-</div>
-<div className="flex flex-col gap-3">
-<h1 className="text-2xl font-medium underline">Vision</h1>
-<h1>  - We envision a world where waste is seen as a valuable resource and renewable energy is accessible to all, driving environmental conservation and social empowerment.</h1></div>
-<div className="flex flex-col gap-3">
-<h1 className="text-2xl font-medium underline">Values</h1>
-<div>
-<h1> - Sustainability: We are committed to promoting sustainable practices and reducing environmental impact.</h1>
-<h1> - Transparency: We prioritize transparency in all our operations, ensuring trust and accountability.</h1>
-<h1> - Collaboration: We believe in the power of collaboration and community engagement to drive positive change.
-</h1>
-</div>
-</div>
-<div className="flex flex-col gap-3">
-<h1 className="text-2xl font-medium underline">Expertise</h1>
-<h1>  - Our team brings together expertise in waste management, renewable energy, blockchain technology, and community development, enabling us to deliver innovative solutions with real-world impact.</h1></div>
-<div className="flex flex-col gap-3">
-<h1 className="text-2xl font-medium underline">Impact</h1>
-<h1>  - With our platform, we aim to make a meaningful impact on the environment, economy, and society, creating a more sustainable and resilient future for generations to come.</h1></div>
-<div className="flex flex-col gap-3">
-<h1 className="text-2xl font-medium underline">Partnerships</h1>
-<h1>  - We collaborate with local governments, waste management authorities, renewable energy providers, and community organizations to maximize our impact and reach.</h1></div>
-<div className="flex flex-col gap-3">
-<h1 className="text-2xl font-medium underline">Commitment to Continuous Improvement</h1>
-<h1>  - We are dedicated to continuous improvement, regularly seeking feedback from users and stakeholders to enhance our platform and services.</h1></div>
-<div className="flex flex-col gap-3">
-<h1 className="text-2xl font-medium underline">Join Us</h1>
-<h1>  - Join us in our journey towards a greener, cleaner future. Together, we can make a difference and create a world where waste is no longer wasted.</h1></div>
-<div>
-    
-</div>
-    </div>
+      <h1 className="text-4xl font-bold underline">About Us</h1>
+      <div className="text-xl flex flex-col gap-8">
+        <div className="flex flex-col gap-3">
+          <h1 className="text-2xl font-medium underline">Mission</h1>
+          <h1>
+            {" "}
+            - Our mission is to revolutionize waste management and energy
+            production by providing an innovative platform that incentivizes
+            sustainable practices and fosters community engagement.
+          </h1>
+        </div>
+        <div className="flex flex-col gap-3">
+          <h1 className="text-2xl font-medium underline">Vision</h1>
+          <h1>
+            {" "}
+            - We envision a world where waste is seen as a valuable resource and
+            renewable energy is accessible to all, driving environmental
+            conservation and social empowerment.
+          </h1>
+        </div>
+        <div className="flex flex-col gap-3">
+          <h1 className="text-2xl font-medium underline">Values</h1>
+          <div>
+            <h1>
+              {" "}
+              - Sustainability: We are committed to promoting sustainable
+              practices and reducing environmental impact.
+            </h1>
+            <h1>
+              {" "}
+              - Transparency: We prioritize transparency in all our operations,
+              ensuring trust and accountability.
+            </h1>
+            <h1>
+              {" "}
+              - Collaboration: We believe in the power of collaboration and
+              community engagement to drive positive change.
+            </h1>
+          </div>
+        </div>
+        <div className="flex flex-col gap-3">
+          <h1 className="text-2xl font-medium underline">Expertise</h1>
+          <h1>
+            {" "}
+            - Our team brings together expertise in waste management, renewable
+            energy, blockchain technology, and community development, enabling
+            us to deliver innovative solutions with real-world impact.
+          </h1>
+        </div>
+        <div className="flex flex-col gap-3">
+          <h1 className="text-2xl font-medium underline">Impact</h1>
+          <h1>
+            {" "}
+            - With our platform, we aim to make a meaningful impact on the
+            environment, economy, and society, creating a more sustainable and
+            resilient future for generations to come.
+          </h1>
+        </div>
+        <div className="flex flex-col gap-3">
+          <h1 className="text-2xl font-medium underline">Partnerships</h1>
+          <h1>
+            {" "}
+            - We collaborate with local governments, waste management
+            authorities, renewable energy providers, and community organizations
+            to maximize our impact and reach.
+          </h1>
+        </div>
+        <div className="flex flex-col gap-3">
+          <h1 className="text-2xl font-medium underline">
+            Commitment to Continuous Improvement
+          </h1>
+          <h1>
+            {" "}
+            - We are dedicated to continuous improvement, regularly seeking
+            feedback from users and stakeholders to enhance our platform and
+            services.
+          </h1>
+        </div>
+        <div className="flex flex-col gap-3">
+          <h1 className="text-2xl font-medium underline">Join Us</h1>
+          <h1>
+            {" "}
+            - Join us in our journey towards a greener, cleaner future.
+            Together, we can make a difference and create a world where waste is
+            no longer wasted.
+          </h1>
+        </div>
+        <div></div>
+      </div>
     </div>
   );
 }
