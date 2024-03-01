ae1030c9bf64ac4700c033cf391af796d1c81755
AAshu1412 <ashutoshsanjeevmittal@gmail.com>
2024-03-02 00:56:29 +0530

    adding send function in dashboard

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -2,10 +2,16 @@ import React from "react";
 import { useState } from "react";
 import water from "../assets/icons8-water-64.svg";
 import electricity from "../assets/icons8-electricity-50.svg";
+import { string } from "prop-types";
 
 export default function Dashboard() {
   const [electricity1, setElectricity] = useState(0);
   const [water1, setWater] = useState(0);
+  const [send_func, setSend_func] = useState({
+    address: "0x295FaF0D270De6d5e9ACDFe287B6844D2335590B",
+    coin: 100,
+    waste: 100,
+  });
 
   const handleInput = (event) => {
     const name = event.target.name;
@@ -14,11 +20,23 @@ export default function Dashboard() {
     console.log(name, value);
     if (name == "elec") setElectricity(value);
     if (name == "wat") setWater(value);
+    // if (name=="user_address") setSend_func({ ...send_func, [name]: value })
+    // if (name=="user_coin") setSend_func({ ...send_func, [name]: value })
+    // if (name=="user_waste") setSend_func({ ...send_func, [name]: value })
+  };
+
+  const handleInput2 = (event) => {
+    const name = event.target.name;
+    // console.log(event.target.value);
+    const value = event.target.value;
+    console.log(name, value);
+
+    setSend_func({ ...send_func, [name]: value });
   };
 
   return (
     <div className=" flex flex-row justify-between">
-      <div className="ml-36 mt-28 ">
+      <div className="ml-36 mt-28 flex flex-col gap-20">
         <div className="flex flex-col gap-10  p-10 border border-4 border-black rounded-2xl">
           <div>
             <div className="flex flex-row gap-44 items-center">
@@ -79,8 +97,47 @@ export default function Dashboard() {
             </div>
           </div>
         </div>
-        <div>dddddddddddddddddddddddddddddddddddddddddddddddddd</div>
+        <div className="flex flex-col gap-10  p-10 border border-4 border-black rounded-2xl">
+          <div className="flex flex-row gap-9">
+            <input
+              placeholder="Address"
+              type="text"
+              autoComplete="off"
+              value={send_func.address}
+              onChange={handleInput2}
+              name="address"
+              id="address"
+              className="h-10 rounded-md border-2 border-black "
+            ></input>
+            <input
+              placeholder="Coin"
+              type="number"
+              autoComplete="off"
+              value={send_func.coin}
+              onChange={handleInput2}
+              name="coin"
+              id="coin"
+              className="h-10 rounded-md border-2 border-black "
+            ></input>
+            <input
+              placeholder="Waste"
+              type="number"
+              autoComplete="off"
+              value={send_func.waste}
+              onChange={handleInput2}
+              name="waste"
+              id="waste"
+              className="h-10 rounded-md border-2 border-black "
+            ></input>
+          </div>
+          <div className="flex flex-row justify-center">
+            <button className="border-2 border-black px-8 py-3 rounded-md font-bold text-lg">
+              Transaction
+            </button>
+          </div>
+        </div>
       </div>
+
       <div className="bg-black border-0 rounded-3xl h-[76vh] mr-7">
         <div className="grid grid-cols-2 gap-8">
           <div className="border-2 bg-white border-black shadow-md shadow-slate-100 py-16 px-2 mt-28 ml-10 grid gap-6  rounded-2xl	 ">
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,16 +1,29 @@
 import React from "react";
-
+import home_img from "../assets/image.png"
+import { useAccount } from "wagmi";
 
 
 export default function Home() {
+    const { address } = useAccount();
 
   return (
-    <div className="bg-blue-200">
-        <div>
-        Cleam Power Credit Program
-        </div>
-        <div>
+    <div className="bg-blue-200 flex flex-row justify-center mt-20 items-center ">
+        <div className="flex flex-col gap-12">
+            <div className="flex flex-col gap-9">
+            <h1 className="w-[750px] text-5xl font-bold">Make a difference every day by recycling waste and powering your community.</h1>    
+        <h1 className="w-[700px] text-xl">Join the movement towards sustainable living with our decentralized waste-to-energy platform – where every action counts towards a greener future.</h1>
+     
+            </div>
+            <div className="flex flex-col gap-2">
+            <h1 className="text-lg">{address ?? "Loading address"}</h1>
+            <h1 className="text-lg">{address ?? "Loading address"}</h1>
 
+            <h1 className="text-lg">{address ?? "Loading address"}</h1>
+
+            </div>
+           </div>
+        <div>
+<img src={home_img}></img>
         </div>
     </div>
   );
